96d887ba90553367b14692a4e6dbf9db7ea1274d
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2024-06-25 14:04:48 +0300

    BaseCommandDto

--- src/BiUM.Core/Base/BaseRequest.cs
+++ src/BiUM.Core/Base/BaseRequest.cs
@@ -2,26 +2,27 @@ using MediatR;
 
 namespace BiUM.Core.Base;
 
-public record BaseRequestDto<TType> : IRequest<ApiResponse<TType>>
+public record BaseRequestDto<TType> : IRequest<TType>
 {
     public required Guid CorrelationId { get; set; }
     public string? LanguageCode { get; set; }
 }
 
-public record BaseCommandDto<TType> : BaseRequestDto<TType>
+public record BaseCommandDto : BaseRequestDto<ApiEmptyResponse>
 {
 }
 
-public record BaseCommandTenantDto<TType> : BaseRequestDto<TType>
+public record BaseCommandTenantDto : BaseCommandDto
 {
     public Guid? TenantId { get; set; }
 }
 
-public record BaseQueryDto<TType> : BaseRequestDto<TType>
+public record BaseQueryDto<TType> : BaseRequestDto<ApiResponse<TType>>
 {
+    public Guid? Id { get; set; }
 }
 
-public record BaseQueryTenantDto<TType> : BaseRequestDto<TType>
+public record BaseQueryTenantDto<TType> : BaseQueryDto<TType>
 {
     public Guid? TenantId { get; set; }
 }
