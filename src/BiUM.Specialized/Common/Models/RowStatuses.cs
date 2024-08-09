55e93deef366196528360159c3ffa6724a9d074b
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2024-08-09 18:30:01 +0300

    TranslationDto added

---
+++ src/BiUM.Specialized/Common/Models/RowStatuses.cs
@@ -0,0 +1,18 @@
+﻿namespace BiUM.Specialized.Common.Models;
+
+public static class RowStatuses
+{
+    public const int Exist = 0;
+    public const int New = 1;
+    public const int Edited = 2;
+    public const int Deleted = 3;
+
+    public static readonly IReadOnlySet<int> All
+        = new HashSet<int>()
+        {
+            Exist,
+            New,
+            Edited,
+            Deleted
+        };
+}
