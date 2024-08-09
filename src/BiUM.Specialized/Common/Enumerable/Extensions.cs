76f00d7dbc61a5c9dd88959e1f570ffea6c69b0c
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2024-08-09 17:49:05 +0300

    New Extensions

---
+++ src/BiUM.Specialized/Common/Enumerable/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using BiUM.Infrastructure.Common.Models;
+
+namespace System.Linq;
+
+public static class Extensions
+{
+    public static string? GetColumnTranslation<TSource>(this IEnumerable<TSource> source, string columnName)
+        where TSource : TranslationBaseEntity
+    {
+        return source.FirstOrDefault(x => x.Column.Equals(columnName))?.Translation;
+    }
+}
--- src/BiUM.Specialized/Database/Extensions.cs
+++ src/BiUM.Specialized/Database/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using BiUM.Specialized.Common.API;
+﻿using AutoMapper;
+using BiUM.Specialized.Common.API;
 using Microsoft.EntityFrameworkCore;
 
 namespace BiUM.Specialized.Database;
@@ -9,11 +10,33 @@ public static class Extensions
     {
         var query = queryable.AsNoTracking();
 
+        var _pageStart = !pageStart.HasValue || pageStart.Value < 0 ? 0 : pageStart.Value;
+        var _pageSize = !pageSize.HasValue || pageSize.Value < 0 ? 10 : pageSize.Value;
+
         return new PaginatedApiResponse<TDestination>(
-            items: await query.Skip(pageStart.Value).Take(pageSize.Value).ToListAsync(cancellationToken),
+            items: await query.Skip(_pageStart).Take(_pageSize).ToListAsync(cancellationToken),
             count: await query.CountAsync(cancellationToken),
-            pageNumber: (pageStart.Value == 0 ? 0 : pageStart.Value / pageSize.Value) + 1,
-            pageSize: pageSize.Value
+            pageNumber: (_pageStart == 0 ? 0 : _pageStart / _pageSize) + 1,
+            pageSize: _pageSize
+        );
+    }
+
+    public static async Task<PaginatedApiResponse<TDestination>> ToPaginatedListAsync<TSource, TDestination>(this IQueryable<TSource> queryable, IMapper mapper, int? pageStart = 0, int? pageSize = 10, CancellationToken cancellationToken = default)
+        where TSource : class
+        where TDestination : class
+    {
+        var query = queryable.AsNoTracking();
+
+        var _pageStart = !pageStart.HasValue || pageStart.Value < 0 ? 0 : pageStart.Value;
+        var _pageSize = !pageSize.HasValue || pageSize.Value < 0 ? 10 : pageSize.Value;
+
+        var items = mapper.Map<List<TDestination>>(await query.Skip(_pageStart).Take(_pageSize).ToListAsync(cancellationToken));
+
+        return new PaginatedApiResponse<TDestination>(
+            items: items,
+            count: await query.CountAsync(cancellationToken),
+            pageNumber: (_pageStart == 0 ? 0 : _pageStart / _pageSize) + 1,
+            pageSize: _pageSize
         );
     }
 }
