b8b8e31862b006effc457b37e0b65e62021d1110
Nusret Parlak @ FunctiOnAir <nusret.parlak@dgpaysit.com>
2024-08-10 03:33:26 +0300

    Fix IMap

--- src/BiUM.Specialized/Common/Models/TranslationDto.cs
+++ src/BiUM.Specialized/Common/Models/TranslationDto.cs
@@ -1,9 +1,13 @@
-﻿namespace BiUM.Specialized.Common.Models;
+﻿using BiUM.Infrastructure.Common.Models;
+using BiUM.Specialized.Mapping;
 
-public class TranslationDto
+namespace BiUM.Specialized.Common.Models;
+
+public class TranslationDto : IMapFrom<TranslationBaseEntity>
 {
     public required Guid Id { get; set; }
     public required Guid LanguageId { get; set; }
+    public string? Column { get; set; }
     public string? Translation { get; set; }
     public int _rowStatus { get; set; }
 }
